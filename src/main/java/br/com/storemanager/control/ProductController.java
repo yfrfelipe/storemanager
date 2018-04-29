5a7bbe9ccf58ae0ff8101511928427076a6fb145
Yrineu Rodrigues <yfrfelipe@gmail.com>
2018-04-29 14:41:41 -0700

    Adding swagger.
    Signed-off-by: Yrineu Rodrigues <yfrfelipe@gmail.com>

--- src/main/java/br/com/storemanager/control/ProductController.java
+++ src/main/java/br/com/storemanager/control/ProductController.java
@@ -2,39 +2,45 @@ package br.com.storemanager.control;
 
 import br.com.storemanager.model.PersonDTO;
 import br.com.storemanager.service.PersonService;
-import java.util.Map;
-import java.util.UUID;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("person")
+@RequestMapping("/person")
+@Api(value = "storemanager", description = "Operations.")
 public class PersonController implements AbstractWebService<PersonDTO> {
 
     @Autowired
     private PersonService personServiceImpl;
 
-    @PostMapping("person")
+    @PostMapping(path = "/people")
     @Override
-    public void doPost(Map<String, PersonDTO> modelDTO) {
-        personServiceImpl.create(modelDTO.values().iterator().next());
+    public void doPost(@RequestBody PersonDTO modelDTO) {
+        personServiceImpl.create(modelDTO);
+    }
+
+    @GetMapping(path = "/people")
+    @ApiOperation(value = "Search for a gicen ID")
+    @Override
+    public void doGet(@PathVariable String id) {
+
     }
 
     @Override
-    public void doGet(UUID id) {
+    public void doPut(PersonDTO modelDTO) {
 
     }
 
     @Override
-    public void doPut(Map<String, PersonDTO> modelDTO) {
-
-    }
-
-    @Override
-    public void doDelete(Map<String, PersonDTO> modelDTO) {
+    public void doDelete(PersonDTO modelDTO) {
 
     }
 
